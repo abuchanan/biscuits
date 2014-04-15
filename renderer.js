e85889e86d0928fb2626441b2e28cac33ad01900
Alex Buchanan <buchanae@gmail.com>
2014-04-14 21:06:40 -0700

    keybindings

--- renderer.js
+++ renderer.js
@@ -8,12 +8,19 @@ function Tile(row, column, value) {
   this.value = value;
 }
 
-function TileCollection(numRows, numColumns) {
+function Grid(numRows, numColumns) {
   this.numRows = numRows;
   this.numColumns = numColumns;
   this._items = [];
+
+  for (var row_i = 0; row_i < numRows; row_i++) {
+    for (var col_i = 0; col_i < numColumns; col_i++) {
+      this.setTile(row_i, col_i, undefined);
+    }
+  }
 }
-TileCollection.prototype = {
+
+Grid.prototype = {
   forEach: function(callback) {
     for (var i = 0, ii = this.numRows; i < ii; i++) {
       for (var j = 0, jj = this.numColumns; j < jj; j++) {
@@ -29,15 +36,16 @@ TileCollection.prototype = {
   },
 };
 
-function SpriteTileRenderer(collection, sprites, tileHeight, tileWidth) {
-  this.collection = collection;
+
+function SpriteTileRenderer(grid, sprites, tileHeight, tileWidth) {
+  this.grid = grid;
   this.tileHeight = tileHeight;
   this.tileWidth = tileWidth;
 }
 
 SpriteTileRenderer.prototype = {
   render: function(ctx) {
-    this.collection.forEach(function(tile) {
+    this.grid.forEach(function(tile) {
       var x = tile.column * this.tileWidth;
       var y = tile.row * this.tileHeight;
       ctx.fillStyle = tile.value;
@@ -47,44 +55,62 @@ SpriteTileRenderer.prototype = {
 };
 
 
-function PlayerRenderer(tileHeight, tileWidth) {
+function PlayerRenderer(player, tileHeight, tileWidth) {
   this.tileHeight = tileHeight;
   this.tileWidth = tileWidth;
-  this.position = {row: 2, column: 1};
+  this.player = player;
 }
 
 PlayerRenderer.prototype = {
   render: function(ctx) {
-    var x = this.position.column * this.tileWidth;
-    var y = this.position.row * this.tileHeight;
-    ctx.fillStyle = 'purple';
+    var x = this.player.position.column * this.tileWidth;
+    var y = this.player.position.row * this.tileHeight;
+    ctx.fillStyle = 'rgba(255, 0, 0, 0.3)';
     ctx.fillRect(x, y, this.tileWidth, this.tileHeight);
   },
 };
 
 
 function startBiscuits(canvas) {
-  var rows = 20;
-  var columns = 20;
 
-  var tiles = new TileCollection(rows, columns);
+  var grid = new Grid(20, 20);
 
-  for (var row_i = 0; row_i < rows; row_i++) {
-    for (var col_i = 0; col_i < columns; col_i++) {
-      tiles.setTile(row_i, col_i, 'gray');
-    }
-  }
+  grid.forEach(function(tile) {
+    tile.value = 'gray';
+  });
 
-
-  tileWidth = canvas.width / tiles.numColumns;
-  tileHeight = canvas.height / tiles.numRows;
+  tileWidth = canvas.width / grid.numColumns;
+  tileHeight = canvas.height / grid.numRows;
   
   var ctx = canvas.getContext('2d');
 
+  var player = {
+    position: {row: 2, column: 1},
+  };
+
   var renderers = [
-    new SpriteTileRenderer(tiles, sprites, tileWidth, tileHeight),
-    new PlayerRenderer(tileWidth, tileHeight),
+    new SpriteTileRenderer(grid, sprites, tileWidth, tileHeight),
+    new PlayerRenderer(player, tileWidth, tileHeight),
   ];
+
+  document.addEventListener('keypress', function(event) {
+    var keyCodeMap = {
+      38: 'UP',
+      40: 'DOWN',
+      37: 'LEFT',
+      39: 'RIGHT',
+    };
+
+    if (event.keyCode == 38) {
+      player.position.row -= 1;
+    } else if (event.keyCode == 40) {
+      player.position.row += 1;
+    } else if (event.keyCode == 37) {
+      player.position.column -= 1;
+    } else if (event.keyCode == 39) {
+      player.position.column += 1;
+    }
+  }, true);
 
   function masterRender() {
     for (var i = 0, ii = renderers.length; i < ii; i++) {
