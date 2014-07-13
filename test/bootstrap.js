d1df0a1eafde1902a70bebed658597674640a504
Alex Buchanan <buchanae@gmail.com>
2014-07-13 14:33:46 +1200

    WIP: custom test runner
    * find test files by path (test/suites/*)
    * find test functions by name (starts with "test")
    * run tests and report to karma
    
    still needs:
    * ability to skip tests
    * run all tests, instead of hard-coded debugging set

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,68 +1,124 @@
-// TODO custom karma reporter
-//TODO file bug with es6-module-loader about instanceof bug
+(function(karma) {
 
-/*
-  System.config({
-    baseURL: '/base/',
+  var allTestFiles = [];
+  var SUITE_REGEXP = /test\/suites\/.*\.js/i;
+  var TEST_FUNC_REGEXP = /^test/;
+
+  var pathToModule = function(path) {
+    return path.replace(/^\/base\//, '').replace(/\.js$/, '');
+  };
+
+  Object.keys(window.__karma__.files).forEach(function(file) {
+    if (SUITE_REGEXP.test(file)) {
+      // Normalize paths to RequireJS module names.
+      allTestFiles.push(pathToModule(file));
+    }
+  });
+
+  // TODO(abuchanan) dev/debugging only
+  allTestFiles = ['test/suites/keyboardInput'];
+
+  console.log(allTestFiles);
+
+  // Extend a given object with all the properties in passed-in object(s).
+  function extend(obj) {
+    Array.prototype.slice.call(arguments, 1).forEach(function(source) {
+      if (source) {
+        for (var prop in source) {
+          obj[prop] = source[prop];
+        }
+      }
+    });
+    return obj;
+  }
+
+  function skipTest(testFunc) {
+    // TODO(abuchanan) support annotation, testFunc.skip = true
+    // TODO(abuchanan) support skip entire suite
+    return false;
+  }
+
+  function runTest(testFunc) {
+    var skipped = false;
+    var success = true;
+    var log = [];
+    var time = 0;
+
+    if (skipTest(testFunc)) {
+      skipped = true;
+    } else {
+      var start = Date.now();
+      try {
+        testFunc();
+      } catch (e) {
+        success = false;
+        log.push(e.stack);
+      }
+      time = Date.now() - start;
+    }
+
+    return {
+      skipped: skipped,
+      success: success,
+      log: log,
+      time: time,
+    };
+  }
+
+  karma.start = function(config) {
+
+    var testNames = [];
+    var tests = [];
+
+    allTestFiles.forEach(function(filename) {
+      var mod = require(filename);
+      for (var key in mod) {
+        if (TEST_FUNC_REGEXP.test(key) && typeof mod[key] == 'function') {
+          testNames.push(key);
+          tests.push({
+            id: key,
+            // TODO description
+            suite: [filename],
+            func: mod[key]
+          });
+        }
+      }
+    });
+
+    karma.info({
+      total: tests.length,
+      specs: testNames,
+    });
+
+    var resultDefaults = {
+      id: 'unknown',
+      description: '',
+      suite: [],
+      log: [],
+      skipped: false,
+      success: false,
+      time: 0,
+    };
+
+    tests.forEach(function(test) {
+      var result = extend({}, resultDefaults, test, runTest(test.func));
+      karma.result(result);
+    });
+
+    karma.complete();
+  };
+
+  require.config({
+    // Karma serves files under /base, which is the basePath from your config file
+    baseUrl: '/base',
+    deps: allTestFiles,
+    callback: karma.start,
+
     map: {
-      'di': 'lib/di.js/index',
+      '*': {
+        'di': 'lib/di.js/index'
+      }
     },
   });
-  //System.baseURL = '/base/';
 
-  window.__karma__.start = function() {
-
-    System
-      .import('test/suites/scenarios/playerMovement')
-      .catch(function(err) {
-        setTimeout(function() {
-          //console.error(err.stack);
-          //console.log(err);
-          //console.log(System.failed);
-          throw err;
-        });
-      });
-  };
-  */
-
-var allTestFiles = [];
-var TEST_REGEXP = /(spec|test)\.js$/i;
-
-var pathToModule = function(path) {
-  return path.replace(/^\/base\//, '').replace(/\.js$/, '');
-};
-
-Object.keys(window.__karma__.files).forEach(function(file) {
-  if (TEST_REGEXP.test(file)) {
-    // Normalize paths to RequireJS module names.
-    allTestFiles.push(pathToModule(file));
-  }
-});
-
-require.config({
-  // Karma serves files under /base, which is the basePath from your config file
-  baseUrl: '/base',
-
-  paths: {
-    //'rtts-assert': './node_modules/rtts-assert/dist/amd/assert'
-  },
-
-  map: {
-    '*': {
-      'di': 'lib/di.js/index'
-    }
-  },
-
-  // dynamically load all test files
-  deps: [
-    'test/suites/keyboardInput',
-    //'test/suites/scenarios/squirrelMovement',
-    //'test/suites/scenarios/sceneLoad',
-    //'test/suites/scenarios/playerMovement',
-  ],
-
-  // we have to kickoff jasmine, as it is asynchronous
-  callback: window.__karma__.start
-});
-
-window.__karma__.start = function() {};
+})(window.__karma__);
--- test/suites/keyboardInput.js
+++ test/suites/keyboardInput.js
@@ -2,27 +2,34 @@ import {Injector} from 'di';
 import {Input, KeyboardInput} from 'src/input';
 import {KeyEventsHelper} from 'test/utils/KeyEventsHelper';
 
-var injector = new Injector();
-var helper = KeyEventsHelper(document);
+export function testKeyboardInput() {
+  var injector = new Injector();
+  var helper = KeyEventsHelper(document);
 
-injector.get(KeyboardInput);
-var input = injector.get(Input);
+  console.log('keyboard');
 
-var keys = [
-  ['Up', 38],
-  ['Down', 40],
-  ['Left', 37],
-  ['Right', 39],
-  ['Use', 69],
-  ['Sword', 70],
-];
+  injector.get(KeyboardInput);
+  var input = injector.get(Input);
 
-for (var i = 0; i < keys.length; i++) {
-  var name = keys[i][0];
-  var code = keys[i][1];
+  var keys = [
+    ['Up', 38],
+    ['Down', 40],
+    ['Left', 37],
+    ['Right', 39],
+    ['Use', 69],
+    ['Sword', 70],
+  ];
 
-  helper.keydown(code);
-  assert(input[name]);
-  helper.keyup(code);
-  assert(!input[name]);
+  for (var i = 0; i < keys.length; i++) {
+    var name = keys[i][0];
+    var code = keys[i][1];
+
+    helper.keydown(code);
+    // TODO import assert lib
+    //      which mean rewriting it to be es6, or at least not attach itself to
+    //      the window.
+    assert(input[name]);
+    helper.keyup(code);
+    assert(!input[name]);
+  }
 }
